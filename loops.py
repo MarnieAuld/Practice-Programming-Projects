53674efc4475da8d461d6332ef9ef810d790857c
Marnie Auld <MarnieLee.Auld@my.jcu.edu.au>
2017-10-27 22:37:10 +1000

    initial commit

---
+++ loops.py
@@ -0,0 +1,26 @@
+"""
+Loops Programming Practice
+"""
+
+"""
+Write a menu-driven program that loops until the user quits. 
+The options are (G)et name, (U)ppercase, (L)owercase, (Q)uit.
+The program should start by asking the user for their name before the menu, but they can (G)et a new name. 
+U/L print the name in either uppercase or lowercase.
+"""
+
+
+"""
+Print the square of numbers between a given range. 
+Ask the user for a lower number and a higher number..
+.. then print all of the squares of the numbers in that range (inclusive).
+Error-check the second number to make sure it is higher than the first.
+"""
+
+
+"""
+Print a "progress bar" based on a percentage (number). 
+E.g. 50% would print something like: |*****-----|, 10% would print |*---------|
+Modify your previous progress bar code so you can specify a width for the bar 
+(if using a function, pass in the percentage and width). E.g. 50% of 18 would be:|*********---------|
+"""
